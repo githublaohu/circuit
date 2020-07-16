6f5a3e54786193a1ba195bc4084230cd7d6afe02
laohu <2372554140@qq.com>
2020-07-16 23:53:15 +0800

    创建core

---
+++ circuit-core/src/main/java/com/lamp/circuit/core/sms/ProxySendSms.java
@@ -0,0 +1,28 @@
+/*
+* frxs Inc.  湖南兴盛优选电子商务有限公司.
+* Copyright (c) 2017-2019. All Rights Reserved.
+*/
+package com.lamp.circuit.core.sms;
+
+import com.lamp.circuit.api.sms.SendSms;
+
+public class ProxySendSms implements SendSms{
+
+    
+    
+    public void sendSms() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void sendBatchSms() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void querySendDetails() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
